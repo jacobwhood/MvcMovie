f0749bbcefccd014e42afd8b8bf8b7c88926e3a1
jacobwhood <jacobwhood@utexas.edu>
2018-04-03 22:23:02 -0500

    Added Movie model and installed Entity Framework

---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        // ID is required as primary key in database
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
