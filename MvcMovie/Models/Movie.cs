9c7b3f2d4f233756c4a4c74cfda7d3f6f6685561
jacobwhood <jacobwhood@utexas.edu>
2018-04-04 21:51:38 -0500

    Add database seed class and functionality

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace MvcMovie.Models
 {
@@ -7,6 +8,9 @@ namespace MvcMovie.Models
         // ID is required as primary key in database
         public int ID { get; set; }
         public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
