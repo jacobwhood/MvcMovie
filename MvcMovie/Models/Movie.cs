443be0db71919ead256553ad09f1a81b63a13747
jacobwhood <jacobwhood@utexas.edu>
2018-04-10 19:28:53 -0500

    Updated Index Movies\Index page to add search

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -7,12 +7,17 @@ namespace MvcMovie.Models
     {
         // ID is required as primary key in database
         public int ID { get; set; }
+
         public string Title { get; set; }
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
         public string Genre { get; set; }
+
         public decimal Price { get; set; }
+
+        public string Rating { get; set; }
     }
 }
